a2df2376b1b0e413b7a5938c8e7aa85eeb5e9223
OneOverX <root1overXcubed@gmail.com>
2022-11-23 20:31:08 -0800

    Adding place holder text until DB finalizes in Cyclic.

--- cumbot.js
+++ cumbot.js
@@ -34,8 +34,8 @@ class CumBot extends Client{
 		return this.channels.cache.get(channel_id);
 	}
 
-	async getLastCumTime(channel_id){
-		try {/*
+	async getLastCumTime(channel_id){/*
+		try {
 			const result = await query(
 				`SELECT last_cum_id 
 				FROM cum 
@@ -47,11 +47,12 @@ class CumBot extends Client{
 			let message = this.getTextChannel(channel_id).messages.cache.get(
 							result.rows.at(0).last_cum_id
 						);
-			return Math.trunc(message.createdAt.getTime()/1000);*/
+			return Math.trunc(message.createdAt.getTime()/1000);
 		} catch (err) {
 			console.error(err);
 			return 0;
-		}
+		}*/
+		return;
 	}
 
 	async maybeRecord(message) {
@@ -97,23 +98,25 @@ class CumBot extends Client{
 
 	async count(channel_id) {
 		try {
-			const result = await query(
+			/*const result = await query(
 				`SELECT cum_count FROM cum WHERE channel_id = ${channel_id}`);
 			if (result.rows.length == 0) {
 				noCumMessage(this.getTextChannel(channel_id));
 				return;
 			}
 			this.getTextChannel(channel_id).send(
-				`Came ${result.rows.at(0).cum_count} times in this channel.`);
+				`Came ${result.rows.at(0).cum_count} times in this channel.`);*/
+			this.getTextChannel(channel_id).send(`Waiting for Cyclic DB to exit preview, remind my creator if want this reimplmented.`);
 		} catch (err) {
 			console.error(err);
 		}
 	}
 
-	async last(channel_id) {
+	async last(channel_id) {/*
 		let time = await this.getLastCumTime(channel_id);
 		if (time == 0) return;
-		this.getTextChannel(channel_id).send(`Last came on <t:${time}>`);
+		this.getTextChannel(channel_id).send(`Last came on <t:${time}>`);*/
+		this.getTextChannel(channel_id).send(`Waiting for Cyclic DB to exit preview, remind my creator if want this reimplmented.`);
 	}
 
 	next(channel_id) {
